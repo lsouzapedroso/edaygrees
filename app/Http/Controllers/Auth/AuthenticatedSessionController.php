3a05932cedcd2be8cc9bdd372c3d3f66b90f30ef
Daniel Volski <danfvdaum3@hotmail.com>
2022-11-15 14:54:20 -0300

    Setting email as required to LoginRequest

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,6 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
-        dd($request); //REMOVE!!!
         $request->authenticate();
 
         $request->session()->regenerate();
